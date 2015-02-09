14c4c8f3d8134174a94c9e4839d84687856d5ced
Conor <conorsg@gmail.com>
2015-02-09 13:32:05 -0600

    initial commit, really

--- R/clean.R
+++ R/clean.R
@@ -9,6 +9,15 @@ init_clean <- function() {
 #
 #
 
+#oss
+clean_oss <- function(data) {
+  names(data) <- slugify(names(data))
+}
+
+#load
+oss <- read.csv("./data/oss-service-report.csv", sep = ";", header = TRUE)
+
+
 #
 #end init_clean
 }
