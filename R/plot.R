95cbcb4a943b0a892ed87811fc48b66fef9e3e25
Conor <conorsg@gmail.com>
2015-02-10 10:09:57 -0600

    oss s&p visitors and timeliness

--- R/plot.R
+++ R/plot.R
@@ -15,19 +15,42 @@ init_plot <- function() {
 oss_sp <- function() {
   d <-  filter(oss, category == "Safety and Permits" | category == "Safety and Permits - Electrical" | category == "Safety and Permits - Inspections" | category == "Safety and Permits - Mechanical" | category == "Safety and Permits - Plan Review")%>%
         group_by(month(datein, label = TRUE), year(datein), category)%>%
-        summarise(n = n())
+        summarise(n = n(), meanwait = mean(timewaited), meanserve = mean(lengthofservice))
 
-  names(d) <- c("month", "year", "category", "n")
+  names(d) <- c("month", "year", "category", "n", "meanwait", "meanserve")
   d$my <- paste(d$month, d$year)
+  d <- arrange(d, year)
   d$my <- factor(d$my, levels = unique(d$my))
 
   ggplot(d, aes(x = my, y = n, group = category, colour = category)) +
   geom_line() +
-  theme(axis.text.x = element_text(angle = 45, hjust = .97)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = .97), legend.position = "top") +
   labs( title = "Visitors by month", x = "Month", y = "Visitors" ) +
-  scale_colour_discrete(name = "Category") + 
+  scale_colour_discrete(name = "Category",
+                        labels = c("S&P", "S&P - Elec", "S&P - Inspec", "S&P - Mech", "S&P - Plan Rev")
+                        ) +
   ggsave("./output/oss-sp.png", width = 10, height = 5.5)
-  cat( style( "Saving safety and permits line chart...\n", fg = 208) )
+  cat( style( "Saving safety and permits visitors line chart...\n", fg = 208) )
+
+  ggplot(d, aes(x = my, y = meanwait, group = category, colour = category)) +
+  geom_line() +
+  theme(axis.text.x = element_text(angle = 45, hjust = .97), legend.position = "top") +
+  labs( title = "Average wait time", x = "Month", y = "Minutes" ) +
+  scale_colour_discrete(name = "Category",
+                        labels = c("S&P", "S&P - Elec", "S&P - Inspec", "S&P - Mech", "S&P - Plan Rev")
+                        ) +
+  ggsave("./output/oss-sp-wait.png", width = 10, height = 5.5)
+  cat( style( "Saving safety and permits mean wait time line chart...\n", fg = 208) )
+
+  ggplot(d, aes(x = my, y = meanserve, group = category, colour = category)) +
+  geom_line() +
+  theme(axis.text.x = element_text(angle = 45, hjust = .97), legend.position = "top") +
+  labs( title = "Average service time", x = "Month", y = "Minutes" ) +
+  scale_colour_discrete(name = "Category",
+                        labels = c("S&P", "S&P - Elec", "S&P - Inspec", "S&P - Mech", "S&P - Plan Rev")
+                        ) +
+  ggsave("./output/oss-sp-service.png", width = 10, height = 5.5)
+  cat( style( "Saving safety and permits mean service time line chart...\n", fg = 208) )
 }
 
 #load
