5954305aef44e7ef06d0672ce0861ec64b78809a
Conor Gaffney <conorsg@gmail.com>
2015-02-20 21:43:55 -0600

    first pass at chart theme

---
+++ R/lib/theme.R
@@ -0,0 +1,17 @@
+#
+# A theme for CS STAT charts
+#
+
+theme_opa <- function() {
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = .97),
+    axis.line = element_line(colour = "grey50"),
+    axis.line.y = element_blank(),
+    panel.background = element_rect(colour = NA, fill = "white"),
+    panel.border = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.grid.major.x = element_blank(),
+    panel.grid.major.y = element_line(colour = "grey50"),
+    legend.key = element_rect(colour = NA, fill = "white")
+    )
+}
