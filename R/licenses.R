be72854d42025f3a0bb7c626b4621474c7e7b929
Conor Gaffney <conorsg@gmail.com>
2015-03-12 17:41:13 -0500

    more schigoda charts

--- R/licenses.R
+++ R/licenses.R
@@ -37,6 +37,8 @@ plotL <- function() {
 #
 #
 
+theme_set(theme_opa())
+
 mech <- function() {
   d <- filter(l, opa_category == "Mechanical") %>%
        group_by(my) %>%
@@ -46,11 +48,38 @@ mech <- function() {
 
   p <- schigoda(d, "my", "value", fill = "variable")
   p <- buildChart(p)
-  ggsave("./output/21-licenses-mech.png", plot = p, width = 10, height = 7.5)
+  ggsave("./output/22-licenses-mech.png", plot = p, width = 10, height = 7.5)
 }
+
+elec <- function() {
+  d <- filter(l, opa_category == "Electrical") %>%
+       group_by(my) %>%
+       summarise(all = n(), oneday = sum(daystoissue <= 1)) %>%
+       filter(!is.na(oneday)) %>%
+       melt()
+
+  p <- schigoda(d, "my", "value", fill = "variable")
+  p <- buildChart(p)
+  ggsave("./output/23-licenses-elec.png", plot = p, width = 10, height = 7.5)
+}
+
+biz <- function() {
+  d <- filter(l, opa_category == "Business") %>%
+       group_by(my) %>%
+       summarise(all = n(), tenday = sum(daystoissue <= 10), fiveday = sum(daystoissue <= 5), oneday = sum(daystoissue <= 1) ) %>%
+       filter(my != "NA NA") %>%
+       melt()
+
+  p <- schigoda(d, "my", "value", fill = "variable")
+  p <- buildChart(p)
+  ggsave("./output/24-licenses-biz.png", plot = p, width = 10, height = 7.5)
+}
+
 
 #execute
 mech()
+elec()
+biz()
 
 #
 #
