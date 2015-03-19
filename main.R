c932ce3e7a250a015aa472f135f2b33450df7e3e
Conor Gaffney <conorsg@gmail.com>
2015-03-18 19:28:40 -0500

    more comments, ugh

--- main.R
+++ main.R
@@ -1,21 +1,21 @@
-#runs the whole kit and kaboodle
+# runs the whole kit and kaboodle
 
 .libPaths("C:\\Rpackages")
 
-#set reporting period
+# get user to set reporting period
 cat("What is the reporting period?\n\n##(Use mmm yyyy format, please)##\n\n")
 period <- readLines("stdin", 1, warn = FALSE)
 
-#initialize
+# initialize
 init <- function(subdir) {
   Rfiles <- list.files(subdir, pattern = "*.R", full.names = TRUE)
   sapply(Rfiles, source)
   cat("Running!\n")
 }
 
-#sequence of script executions
+# sequence of script executions
 init("R/lib")
 init("R")
 
-#finish
+# finish
 cat("Finished!")
